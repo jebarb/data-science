a61cc3f62aacec112028a8b4c9e9a2126220fa35
James Barbour <jebarbou@cs.unc.edu>
2017-01-26 22:52:57 -0500

    assn1 done, polish

--- lab3/jebarbou_lab3.R
+++ lab3/jebarbou_lab3.R
@@ -1,18 +1,26 @@
+# James Barbour
+
+# even_odd ----------------------------------------------------------------
 even_odd <- function(x) 
   !(trunc(x) %% 2)
 
+# round_good --------------------------------------------------------------
 round_good <- function(x, digits)
   if (x >= 0) floor(x*10^digits + .5)/10^digits else ceiling(x*10^digits - .5)/10^digits
 
+# symdiff -----------------------------------------------------------------
 symdiff <- function(a, b) 
   c(a[!(a %in% b)], b[!(b %in% a)])
 
+# even_sum ----------------------------------------------------------------
 even_sum <- function(vec) 
   sum(vec[even_odd(vec)])
 
+# odd_sum -----------------------------------------------------------------
 odd_sum <- function(vec, even = TRUE) 
   sum(vec[even == even_odd(vec)])
 
+# monkey ------------------------------------------------------------------
 monkey <- function(chars) {
   
 }
