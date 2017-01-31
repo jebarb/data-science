0b45f1008527f8d77fd8ff75494a30838b48f406
James Barbour <jebarbou@cs.unc.edu>
2017-01-31 15:23:07 -0500

    assn1 complete

--- lab3/jebarbou_lab3.R
+++ lab3/jebarbou_lab3.R
@@ -22,5 +22,25 @@ odd_sum <- function(vec, even = TRUE)
 
 # monkey ------------------------------------------------------------------
 monkey <- function(chars) {
-  
+  letter <- c(letters, c(' ', ',', '!'))
+  characters = c(rep('', chars))
+  # generate vector of random chars from set
+  for (i in 1:(chars))
+    characters[i] <- letter[as.integer(runif(1, min = 1, max = 30))]
+  # check conditions
+  for (i in 1:(chars)) {
+    # check i after e except after c
+    if (characters[i] == 'e') {
+      if (characters[i+1] == 'i') {
+        if (characters[i-1] != 'c') {
+          characters[i] <- 'i'
+          characters[i+1] <- 'e'
+        }
+      }
+    }
+    # ensure space after punctuation
+    if (characters[i] %in% c(',', '!'))
+      characters[i+1] = ' '
+  }
+  return(paste(characters, collapse = ''))
 }
